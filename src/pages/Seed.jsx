81974d92128be427d1bdf04256a08d508130b4ca
Joy Kwok <lianglilnf-1996@hotmail.com>
2022-04-02 11:52:44 +0800

    Seed

---
+++ src/pages/Seed.jsx
@@ -0,0 +1,16 @@
+fetch('http://localhost:8000/api/holidays/seed')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+  const seed = []
+
+function Seed() {
+  return (
+    <>
+      <h1>Seed</h1>
+      <pre>{JSON.stringify(seed, null, 2)}</pre>
+    </>
+  );
+}
+
+export default Seed;
